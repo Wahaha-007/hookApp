96cdb4dd357a963890a38df82e117e571a097db7
Wahaha <b0464221@gmail.com>
2022-05-10 21:22:14 +0700

    Finish state with Form

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'almost home', id: 1 },
+    { title: 'memory gospel', id: 2 },
+    { title: 'this wild darkness', id: 3 }
+  ]);
+
+  const addSong = (title) => {
+    setSongs((prev)=>[...prev, { title  , id: uuidv4() }]);
+  };
+
   return (
-    <div classname="song-list">
+    <div className="song-list">
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map(song => {
+          return ( <li key={song.id}>{song.title}</li> );
+        })}
       </ul>
+      <NewSongForm addSong={addSong} />
     </div>
   );
 }
- 
+
 export default SongList;
